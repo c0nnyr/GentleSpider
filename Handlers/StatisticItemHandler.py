f987a660b59145b6dab3f4f15695613deb92783a
c0nny <caojunatthu@163.com>
2017-05-10 08:04:50 +0800

    fix statistic

--- Handlers/StatisticItemHandler.py
+++ Handlers/StatisticItemHandler.py
@@ -8,6 +8,23 @@ class StatisticItemHandler(BaseItemHandler):
 
 	LOG_DURATION = 60#每分钟记录一下
 
+	POST_TEMPLATE = {
+		'cd':
+			{
+				'HouseItem':'成都二手房{meta_district}-{meta_area}-{meta_price_level}: {count}',
+				'CommunityItem':'成都小区{meta_district}-{meta_price_level}: {count}',
+				'NewCommunityItem':'成都新盘{meta_district}: {count}',
+				'DealItem':'成都成交{meta_district}-{meta_area}-{meta_price_level}: {count}',
+			},
+		'hz':
+			{
+				'HouseItem':'杭州二手房{meta_district}-{meta_area}-{meta_price_level}: {count}',
+				'CommunityItem':'杭州小区{meta_district}-{meta_price_level}: {count}',
+				'NewCommunityItem':'杭州新盘{meta_district}: {count}',
+				'DealItem':'杭州成交{meta_district}-{meta_area}-{meta_price_level}: {count}',
+			}
+	}
+
 	def __init__(self):
 		super(StatisticItemHandler, self).__init__()
 		self.statistic = collections.defaultdict(lambda:0)
@@ -15,6 +32,7 @@ class StatisticItemHandler(BaseItemHandler):
 		self.last_log_time = None
 		self.poster = MessagePostManager()
 		self.cur_post_ind = 0
+		self.template = None
 
 	def open_spider(self):
 		self.start_time = time.time()
@@ -41,10 +59,24 @@ class StatisticItemHandler(BaseItemHandler):
 		cur_time = time.time()
 		post_ind = int((cur_time - self.start_time) / 3600 / 2)#没两个小时通知一次
 		if post_ind > self.cur_post_ind or item is None:
-			if item:
-				msg = 'Current find total items {}, current meta {}, {}'.format(sum(self.statistic.itervalues()), item.__dict__, self.statistic)
-			else:
-				msg = 'Current find total items {}, {}'.format(sum(self.statistic.itervalues()), self.statistic)
+			msg = ''
+			try:
+				if item:
+					city = 'cd' if 'cd.lianjia' in item.url else 'hz'
+					templates = self.POST_TEMPLATE[city]
+					cls_name = item.__class__.__name__
+					if cls_name not in templates:
+						return
+					self.template = templates[cls_name]
+					msg = self.template.format(count=dict(self.statistic), **item.__dict__)
+				else:
+					if self.template:
+						msg = self.template.format(count=dict(self.statistic), meta_district='END', meta_area='END', meta_price_level='END')
+			except:
+				if item:
+					msg = 'Current find total items {}, current meta {}'.format(sum(self.statistic.itervalues()), item.__dict__, self.statistic)
+				else:
+					msg = 'Current find total items {}'.format(sum(self.statistic.itervalues()), self.statistic)
 			title = 'Spider statistic'
 			self.poster.post_immediatly(msg, title)
 			self.cur_post_ind = post_ind
